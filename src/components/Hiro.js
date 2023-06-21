f65e1e9c8f0d8c1968ba4debef3e71cb9f558da3
H4rsh4nk <pathakharsh13@gmail.com>
2023-06-21 03:20:54 -0400

    Updating portfolio 	new file:   build/asset-manifest.json 	new file:   build/favicon.ico 	new file:   build/favicon.png 	new file:   build/faviconbak.ico 	new file:   build/index.html 	new file:   build/logo192.png 	new file:   build/logo512.png 	new file:   build/manifest.json 	new file:   build/robots.txt 	new file:   build/static/css/main.1c5c112d.css 	new file:   build/static/css/main.1c5c112d.css.map 	new file:   build/static/js/787.a7249224.chunk.js 	new file:   build/static/js/787.a7249224.chunk.js.map 	new file:   build/static/js/main.e31c9792.js 	new file:   build/static/js/main.e31c9792.js.LICENSE.txt 	new file:   build/static/js/main.e31c9792.js.map 	new file:   build/static/media/C++.9de9df950e692f5576c1.png 	new file:   build/static/media/ansible.b7e5786c9eef61e4ba42a993d20107c6.svg 	new file:   build/static/media/aws.302b8b7756f237c7429273e303078e7a.svg 	new file:   build/static/media/aws.7b8623875202b1a120a5.jpeg 	new file:   build/static/media/bash.9e769001e7aaa8f0e6d77c541525677e.svg 	new file:   build/static/media/carbon.51e3e8df810a866b8281.png 	new file:   build/static/media/cisco.d1403551b12b6376a0f1e1b3973d19b7.svg 	new file:   build/static/media/curve-hr.cbdc8e90bbbff51174494b17a5db6755.svg 	new file:   build/static/media/docker.fec5c368807b57ea14a3f991f894c53d.svg 	new file:   build/static/media/express.ea97b712340ffb8f11d85ccb96309507.svg 	new file:   build/static/media/git.8497c3c4218d87dc7fd569a5549d60d0.svg 	new file:   build/static/media/gitlab.e8e2109489324972d0bce8491d9f36c0.svg 	new file:   build/static/media/java-svgrepo-com.68585ce8dadbd8ecebdc99a764c5c1cf.svg 	new file:   build/static/media/javascript.1d3d28798f08ea1ed7cd47f812044c4f.svg 	new file:   build/static/media/kubernetes.44d2e074b50592a1e5050b2713afd82c.svg 	new file:   build/static/media/kvm.1f706ce94026b9558de284aa1f46657e.svg 	new file:   build/static/media/linux.32a9bcd0350f51b5188852a775332b6c.svg 	new file:   build/static/media/logo.0de5eeb507ac199b911c4916f2e3cb58.svg 	new file:   build/static/media/mikrotik.d2e17a6764779d1cc982433d85aeeb58.svg 	new file:   build/static/media/mongodb-icon.0781b723446e1f8a1734f7831a9c3313.svg 	new file:   build/static/media/mysql-icon.4c50926040e5a77a27dba211addc08e3.svg 	new file:   build/static/media/nodejs.cc3d45a160579a330a5c17cc2c8f1407.svg 	new file:   build/static/media/postgresql.2b6391161de27749b067.png 	new file:   build/static/media/python.d555a447a553a2ed0dba.jpeg 	new file:   build/static/media/python.e78f188fa5e62ee2613d19802f817df6.svg 	new file:   build/static/media/react.19465659ecf7a1122618d3dcf23bf337.svg 	new file:   build/static/media/resume.f90c6cac15d864f7473b.pdf 	new file:   build/static/media/slick.2630a3e3eab21c607e21.svg 	new file:   build/static/media/slick.295183786cd8a1389865.woff 	new file:   build/static/media/slick.a4e97f5a2a64f0ab1323.eot 	new file:   build/static/media/slick.c94f7671dcc99dce43e2.ttf 	new file:   build/static/media/tailwind.7817889fda1887000f0e712239d42b5a.svg 	new file:   build/static/media/windows.7246ccf28ddce71d8cfd11c1ffc38c86.svg 	modified:   package.json 	modified:   src/components/Hiro.js

--- src/components/Hiro.js
+++ src/components/Hiro.js
@@ -13,13 +13,13 @@ export default function Hiro () {
     return (
       <>
         {loaded ? (
-          <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-white flex flex-col items-center justify-center">
+          <div className="fixed top-0  left-0 right-0 bottom-0 w-full z-50 overflow-hidden bg-white flex flex-col items-center justify-center">
             Loading...
           </div>
         ) : null}
         <div
           id="home"
-          className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-white relative"
+          className="flex w-full flex-col mt-10 pt-10 md:flex-row gap-5 items-center justify-center text-white relative"
         >
           <div className="md:w-3/6 md:p-4">
             <img
@@ -42,10 +42,18 @@ export default function Hiro () {
               <h1 className="text-2xl font-bold">Harshank Pathak</h1>
               <p class="text-xl font-bold text-gray-300">Software Engineer</p>
               <p className="text-md font-light text-gray-400 ">
-                            Your friendly neighbourhood IT guy. 
-                                       
-                            Enthusiastic and motivated self-learner. Interested in exploring Cloud Computing, Fullstack, Software Development and Gaming
-                .
+              I'm a software developer who spends most of their day staring at a computer screen and typing code until my fingers ache. But hey, I'm not complaining - I get to make cool stuff and solve problems all day long.
+<br/>
+<br/>
+I'm the proud creator of some of the world's most beautifully crafted code (at least, that's what my mom tells me). When I'm not busy fixing bugs or making software that impresses my colleagues, you can find me trying to remember what the sun looks like or debating the merits of using tabs versus spaces.
+<br/>
+<br/>
+My skills include everything from Java to JavaScript, Python to PHP, and even a bit of C++ for good measure. I'm a jack-of-all-trades when it comes to software development, but a master of none (yet).
+<br/>
+<br/>
+So if you're looking for a software developer who can bring a sense of humor to your team, then look no further. I may not be the funniest person in the world, but I promise to bring a smile to your face every time you see my code.
+     <br/>
+<br/>
               </p>
             </div>
             {/* <a href="http://blog.dama.ink" className="mt-2 block">
@@ -83,8 +91,8 @@ export default function Hiro () {
               </li>
             </ul>
           </div>
-          <img src={hr} className="w-full md:h-2 absolute bottom-0" alt="hr" />
         </div>
+          <img src={hr} className="flex w-full md:h-2 mt-10" alt="hr" />
       </>
     );
 }
